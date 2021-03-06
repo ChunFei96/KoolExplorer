3a2ec6b2d7bd5f75a0cab92a5fc3caaff1ef97fc
= <wenjun.tan@anacle.com>
2021-03-06 17:45:59 +0800

    update Pre-Schools html page

--- KoolExplorer/Pages/Search.cshtml.cs
+++ KoolExplorer/Pages/Search.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAL;
+using DAL.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,22 @@ namespace KoolExplorer.Pages
 {
     public class SearchPageModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public List<ProcessedPreSchool> PreSchools;
+
+        public SearchPageModel(IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+        {
+            _unitOfWork = unitOfWork;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public void OnGet()
         {
+            PreSchools = _unitOfWork.ProcessedPreSchoolRepository.GetAll().ToList();
+            var ma = 0;
+
+            //http://fooplugins.github.io/FooTable/docs/examples/advanced/ajax.html
         }
     }
 }
