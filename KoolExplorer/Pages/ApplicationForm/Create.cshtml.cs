335af891b14a8afa9cde8488f9387b5419494ef4
ChunFei96 <chunfei96@hotmail.com>
2021-03-08 10:56:54 +0800

    Removed dummy data for application creation

--- KoolExplorer/Pages/ApplicationForm/Create.cshtml.cs
+++ KoolExplorer/Pages/ApplicationForm/Create.cshtml.cs
@@ -42,41 +42,6 @@ namespace KoolExplorer.Pages.ApplicationForm
             parentsParticulars = new ParentsParticularsViewModel();
             childsParticulars = new ChildsParticularsViewModel();
 
-            // dummy data
-            //generalInformationViewModel.Area = "West";
-            //generalInformationViewModel.District = "Clementi";
-            //generalInformationViewModel.PreSchool = "First School";
-            //generalInformationViewModel.Programme = "K1";
-            //generalInformationViewModel.ProgrammeTime = "Full-Day";
-            //generalInformationViewModel.StartPeriod = "2021/10/08";
-
-            parentsParticulars.Name1 = "Parent Name 1";
-            parentsParticulars.PassportNo1 = "SJ910293";
-            parentsParticulars.MobileNo1 = "90182932";
-            parentsParticulars.Email1 = "parent1@gmail.com";
-
-            parentsParticulars.Name2 = "Parent Name 2";
-            parentsParticulars.PassportNo2 = "GN919283";
-            parentsParticulars.MobileNo2 = "8891273";
-            parentsParticulars.Email2 = "parent2@gmail.com";
-
-            parentsParticulars.HomeTelNo = "69102932";
-            parentsParticulars.StreetName = "Clementi Ave 6";
-
-            parentsParticulars.BlockNo = "blk 120";
-            parentsParticulars.FloorNo = "09";
-            parentsParticulars.UnitNo = "51";
-
-            parentsParticulars.BuildingName = "Clementi";
-
-            parentsParticulars.PostalCode = "120202";
-            parentsParticulars.HouseholdIncome = "$4001 - $4500";
-
-            childsParticulars.Name = "Child Name";
-            childsParticulars.BirthCertNo = "A910293";
-            //childsParticulars.DOB = "1996/06/04";
-            //childsParticulars.Gender = Core.Expansion.Enum.Gender.Male;
-
             //Init dropdownlist
             generalInformationViewModel.AreaList = await  _dropDownService.GetDropDownByType("Area");
         }
